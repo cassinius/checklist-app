d7ccde09d6b9294d2ac19a672e96339e68d3805c
Bernd Malle <bernd.malle@gmail.com>
2025-04-27 12:36:11 +0800

    init commit with sveltekit, tw & daisy

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="px-1.5 py-1">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-3xl text-orange-500">Welcome to Checklists</h1>
